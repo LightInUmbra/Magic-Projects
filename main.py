8cfbb7a1ed2f7e3a9da34ada9ac783aa83181e75
Umbra Ortiz <56861742+LightInUmbra@users.noreply.github.com>
2026-04-03 20:08:28 -0400

    Changes to main.py

--- main.py
+++ main.py
@@ -15,24 +15,40 @@ HEADERS = {
     
 }
 
-# Searches for exact card, then prints
+# Searches for exact card, then prints name, set code, and rarity
 card_pulled = sf.search_exact_card("The One Ring")
 print(card_pulled)
 
-# Gets information on all printings of one card
+"""
+Gets information on all printings of one card
+Output: Card Name: [card name]
+
+Set: Set name
+Pricing in each currency/availability
+"""
 all_printings = sf.get_all_printings("voja jaws of the conclave")
 print(all_printings)
 
-# Searches for cards with One Ring in card text
+"""
+Searches for cards with Ghalta in card text AND counts how many cards it found
+Output: Card name, set code, rarity
+Output: Number of cards it found
+"""
 card_list = sf.search_cards("Ghalta")
 card_count = sf.card_search_count(card_list)
 print(card_list)
 print(f"\n", card_count)
 
-# Using card_list, shows card name and card colors/identity
+"""
+Using card_list, shows card name and card colors/identity
+Output: Card Name: Color(s)
+"""
 card_list_colors = sf.list_color_identity(card_list)
 print(card_list_colors, "\n")
 
-# gets card prices and displays them for each card in list
+"""
+gets card prices and displays them for each card in list
+Output: Card Name: Price (USD Nonfoil assumed unless specified)
+"""
 card_pricing = sf.print_card_prices(card_list, price_type='price_usd')
 print(card_pricing, "\n")
